83d2359043fcd25ce82870a6f014a7608d29b639
gabrielbabbler <gabriel.babler@sensedia.com>
2022-01-29 19:14:47 -0300

    add annotations

--- src/main/kotlin/com/gbabler/msgoals/model/domain/GoalDomain.kt
+++ src/main/kotlin/com/gbabler/msgoals/model/domain/GoalDomain.kt
@@ -1,16 +1,23 @@
 package com.gbabler.msgoals.model.domain
 
 import com.gbabler.msgoals.enumeration.GoalType
+import org.hibernate.annotations.GenericGenerator
 import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.Table
 
 @Entity
+@Table(name = "goals")
 class GoalDomain(
     @Id
-    @GeneratedValue
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     val id: String,
     val description: String,
+    @Enumerated(EnumType.STRING)
     val type: GoalType,
     val isCompleted: Boolean) {
 
