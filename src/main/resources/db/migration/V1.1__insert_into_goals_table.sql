5e20664e2cf66f833c13fd3324df849b29270a3d
gabrielbabbler <gabriel.babler@sensedia.com>
2022-01-29 19:15:39 -0300

    add new insert script

---
+++ src/main/resources/db/migration/V1.1__insert_into_goals_table.sql
@@ -0,0 +1,12 @@
+INSERT INTO goals(id, description, type, is_completed)
+VALUES
+    ('24bf2487-9e96-40a6-b1f7-dfe5ad91c131', 'Ir na academia 150x no ano', 'PERSONAL', false),
+    ('ec45afff-ca6a-4701-accb-4fc85b1b31dd', 'Não beber refrigerante', 'PERSONAL', false),
+    ('b9af2ec9-dcc9-41c5-8304-2e0942aec1f2', 'Certificação JAVA', 'PROFESSIONAL', false),
+    ('682e5644-27e8-41e5-ac93-fe6d6824b343', 'Criar workshop para ajudar as pessoas', 'PROFESSIONAL', false),
+    ('e189bdde-a1dd-4638-9561-2028af66d65f', 'Não comer doce', 'PERSONAL', false),
+    ('4cce0968-d2fa-48b7-b4c0-7f2f7fdeadc3', 'Doar roupas que não uso mais', 'PERSONAL', true),
+    ('ccee7a33-8ea1-44e5-b0e7-66e1213703f3', 'Doar sangue', 'PERSONAL', true),
+    ('adaf79d1-9e2e-47e4-9f3e-c88d3d3463f4', 'Assistir 10 filmes em inglês, para melhorar o inglês', 'PROFESSIONAL', false),
+    ('12ac10b0-ad79-4ee1-a492-dbc5d8698e65', 'Escrever 10 artigos sobre tecnologia', 'PROFESSIONAL', false),
+    ('72529247-4d01-4cd8-9c32-aff7eb8703da', 'Conversar com alguém em inglês 1x por semana por 3 meses', 'PROFESSIONAL', false);
