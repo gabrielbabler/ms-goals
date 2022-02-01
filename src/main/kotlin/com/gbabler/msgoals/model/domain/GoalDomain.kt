ba48b7332263c103a20de3a658d95a01eb060e8c
gabrielbabbler <gabriel.babler@sensedia.com>
2022-01-31 23:52:29 -0300

    create method to convert

--- src/main/kotlin/com/gbabler/msgoals/model/domain/GoalDomain.kt
+++ src/main/kotlin/com/gbabler/msgoals/model/domain/GoalDomain.kt
@@ -1,6 +1,7 @@
 package com.gbabler.msgoals.model.domain
 
 import com.gbabler.msgoals.enumeration.GoalType
+import com.gbabler.msgoals.model.dto.GoalRequest
 import org.hibernate.annotations.GenericGenerator
 import javax.persistence.Entity
 import javax.persistence.EnumType
@@ -11,14 +12,19 @@ import javax.persistence.Table
 
 @Entity
 @Table(name = "goals")
-class GoalDomain(
+data class GoalDomain(
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    val id: String,
-    val description: String,
+    var id: String?,
+    var description: String,
     @Enumerated(EnumType.STRING)
-    val type: GoalType,
-    val isCompleted: Boolean) {
-
+    var type: GoalType,
+    var isCompleted: Boolean
+) {
+    companion object {
+        fun valueOf(goalRequest: GoalRequest): GoalDomain {
+            return GoalDomain(null, goalRequest.description, goalRequest.type, goalRequest.isCompleted)
+        }
+    }
 }
